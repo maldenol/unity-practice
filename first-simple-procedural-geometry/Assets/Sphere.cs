5b33465d6da20c99fdf510641bed0fca174e2784
maldenol <mineden2300mo@gmail.com>
2022-01-21 15:13:26 +0200

    Add UV mapping

--- first-simple-procedural-geometry/Assets/Sphere.cs
+++ first-simple-procedural-geometry/Assets/Sphere.cs
@@ -11,8 +11,11 @@ public class Sphere : MonoBehaviour {
         mesh.name = "Procedural Sphere";
 
         // Level-Of-Details (count of quads along one side)
-        float step  = 1.0f / LOD;       // discrete quad's side size (optimization)
-        int quadLOD = (int)(LOD * LOD); // LOD^2 (optimization)
+        float xyQuadSize = 1f / LOD;          // discrete quad's side xy size (optimization)
+        float uQuadSize  = 1f / 4f / LOD;     // discrete quad's side u size (optimization)
+        float vQuadSize  = 1f / 3f / LOD;     // discrete quad's side v size (optimization)
+        float halfSize   = 1f / 2f;           // hald of the SIZE (optimization)
+        int quadLOD      = (int)(LOD * LOD);  // LOD^2 (optimization)
 
         int quadsPerSideCount     = quadLOD;
         int verticesPerSideCount  = 4 * quadLOD;
@@ -22,22 +25,32 @@ public class Sphere : MonoBehaviour {
 
         Vector3[] vertices = new Vector3[verticesCount];
         Vector3[] normals  = new Vector3[verticesCount];
+        Vector2[] uvs      = new Vector2[verticesCount];
         int[] triangles    = new int[trianglesCount];
 
         // making cube for further transformation
         for (int j = 0; j < 6; ++j) { // the whole cube
             for (int i = 0; i < quadsPerSideCount; ++i) { // single side
-                int row    = i / (int)LOD;
-                int column = i % (int)LOD;
-                float lx   = column * step - 0.5f;
-                float rx   = (column + 1) * step - 0.5f;
-                float uy   = (row + 1) * step - 0.5f;
-                float dy   = row * step - 0.5f;
+                int row        = i / (int)LOD;
+                int column     = i % (int)LOD;
+                float leftX    = column * xyQuadSize - halfSize;
+                float rightX   = (column + 1) * xyQuadSize - halfSize;
+                float upY      = (row + 1) * xyQuadSize - halfSize;
+                float downY    = row * xyQuadSize - halfSize;
+                float leftU    = column * uQuadSize;
+                float rightU   = (column + 1) * uQuadSize;
+                float upV      = (row + 1) * vQuadSize;
+                float downV    = row * vQuadSize;
 
-                Vector3 lu = new Vector3(lx, uy, 0);
-                Vector3 ru = new Vector3(rx, uy, 0);
-                Vector3 ld = new Vector3(lx, dy, 0);
-                Vector3 rd = new Vector3(rx, dy, 0);
+                Vector3 lu = new Vector3(leftX, upY, 0);
+                Vector3 ru = new Vector3(rightX, upY, 0);
+                Vector3 ld = new Vector3(leftX, downY, 0);
+                Vector3 rd = new Vector3(rightX, downY, 0);
+
+                Vector2 luUV = new Vector2(leftU, upV);
+                Vector2 ruUV = new Vector2(rightU, upV);
+                Vector2 ldUV = new Vector2(leftU, downV);
+                Vector2 rdUV = new Vector2(rightU, downV);
 
                 // placing the side on its place
                 switch (j) {
@@ -47,10 +60,19 @@ public class Sphere : MonoBehaviour {
                     ld = Quaternion.AngleAxis(90, Vector3.up) * ld;
                     rd = Quaternion.AngleAxis(90, Vector3.up) * rd;
 
-                    lu.x -= 0.5f;
-                    ru.x -= 0.5f;
-                    ld.x -= 0.5f;
-                    rd.x -= 0.5f;
+                    lu.x -= halfSize;
+                    ru.x -= halfSize;
+                    ld.x -= halfSize;
+                    rd.x -= halfSize;
+
+                    luUV.x += 0f / 4f;
+                    luUV.y += 1f / 3f;
+                    ruUV.x += 0f / 4f;
+                    ruUV.y += 1f / 3f;
+                    ldUV.x += 0f / 4f;
+                    ldUV.y += 1f / 3f;
+                    rdUV.x += 0f / 4f;
+                    rdUV.y += 1f / 3f;
                     break;
                 case 1: // x+
                     lu = Quaternion.AngleAxis(-90, Vector3.up) * lu;
@@ -58,10 +80,19 @@ public class Sphere : MonoBehaviour {
                     ld = Quaternion.AngleAxis(-90, Vector3.up) * ld;
                     rd = Quaternion.AngleAxis(-90, Vector3.up) * rd;
 
-                    lu.x += 0.5f;
-                    ru.x += 0.5f;
-                    ld.x += 0.5f;
-                    rd.x += 0.5f;
+                    lu.x += halfSize;
+                    ru.x += halfSize;
+                    ld.x += halfSize;
+                    rd.x += halfSize;
+
+                    luUV.x += 2f / 4f;
+                    luUV.y += 1f / 3f;
+                    ruUV.x += 2f / 4f;
+                    ruUV.y += 1f / 3f;
+                    ldUV.x += 2f / 4f;
+                    ldUV.y += 1f / 3f;
+                    rdUV.x += 2f / 4f;
+                    rdUV.y += 1f / 3f;
                     break;
                 case 2: // y-
                     lu = Quaternion.AngleAxis(-90, Vector3.right) * lu;
@@ -69,10 +100,19 @@ public class Sphere : MonoBehaviour {
                     ld = Quaternion.AngleAxis(-90, Vector3.right) * ld;
                     rd = Quaternion.AngleAxis(-90, Vector3.right) * rd;
 
-                    lu.y -= 0.5f;
-                    ru.y -= 0.5f;
-                    ld.y -= 0.5f;
-                    rd.y -= 0.5f;
+                    lu.y -= halfSize;
+                    ru.y -= halfSize;
+                    ld.y -= halfSize;
+                    rd.y -= halfSize;
+
+                    luUV.x += 1f / 4f;
+                    luUV.y += 0f / 3f;
+                    ruUV.x += 1f / 4f;
+                    ruUV.y += 0f / 3f;
+                    ldUV.x += 1f / 4f;
+                    ldUV.y += 0f / 3f;
+                    rdUV.x += 1f / 4f;
+                    rdUV.y += 0f / 3f;
                     break;
                 case 3: // y+
                     lu = Quaternion.AngleAxis(90, Vector3.right) * lu;
@@ -80,16 +120,34 @@ public class Sphere : MonoBehaviour {
                     ld = Quaternion.AngleAxis(90, Vector3.right) * ld;
                     rd = Quaternion.AngleAxis(90, Vector3.right) * rd;
 
-                    lu.y += 0.5f;
-                    ru.y += 0.5f;
-                    ld.y += 0.5f;
-                    rd.y += 0.5f;
+                    lu.y += halfSize;
+                    ru.y += halfSize;
+                    ld.y += halfSize;
+                    rd.y += halfSize;
+
+                    luUV.x += 1f / 4f;
+                    luUV.y += 2f / 3f;
+                    ruUV.x += 1f / 4f;
+                    ruUV.y += 2f / 3f;
+                    ldUV.x += 1f / 4f;
+                    ldUV.y += 2f / 3f;
+                    rdUV.x += 1f / 4f;
+                    rdUV.y += 2f / 3f;
                     break;
                 case 4: // z-
-                    lu.z -= 0.5f;
-                    ru.z -= 0.5f;
-                    ld.z -= 0.5f;
-                    rd.z -= 0.5f;
+                    lu.z -= halfSize;
+                    ru.z -= halfSize;
+                    ld.z -= halfSize;
+                    rd.z -= halfSize;
+
+                    luUV.x += 3f / 4f;
+                    luUV.y += 1f / 3f;
+                    ruUV.x += 3f / 4f;
+                    ruUV.y += 1f / 3f;
+                    ldUV.x += 3f / 4f;
+                    ldUV.y += 1f / 3f;
+                    rdUV.x += 3f / 4f;
+                    rdUV.y += 1f / 3f;
                     break;
                 case 5: // z+
                     lu = Quaternion.AngleAxis(180, Vector3.up) * lu;
@@ -97,10 +155,19 @@ public class Sphere : MonoBehaviour {
                     ld = Quaternion.AngleAxis(180, Vector3.up) * ld;
                     rd = Quaternion.AngleAxis(180, Vector3.up) * rd;
 
-                    lu.z += 0.5f;
-                    ru.z += 0.5f;
-                    ld.z += 0.5f;
-                    rd.z += 0.5f;
+                    lu.z += halfSize;
+                    ru.z += halfSize;
+                    ld.z += halfSize;
+                    rd.z += halfSize;
+
+                    luUV.x += 1f / 4f;
+                    luUV.y += 1f / 3f;
+                    ruUV.x += 1f / 4f;
+                    ruUV.y += 1f / 3f;
+                    ldUV.x += 1f / 4f;
+                    ldUV.y += 1f / 3f;
+                    rdUV.x += 1f / 4f;
+                    rdUV.y += 1f / 3f;
                     break;
                 }
 
@@ -111,6 +178,11 @@ public class Sphere : MonoBehaviour {
                 vertices[vertexOffset + 1] = ru;
                 vertices[vertexOffset + 2] = ld;
                 vertices[vertexOffset + 3] = rd;
+
+                uvs[vertexOffset]     = luUV;
+                uvs[vertexOffset + 1] = ruUV;
+                uvs[vertexOffset + 2] = ldUV;
+                uvs[vertexOffset + 3] = rdUV;
 
                 triangles[triangleOffset]     = vertexOffset;     // left-up quad
                 triangles[triangleOffset + 1] = vertexOffset + 1; // left-up quad
@@ -129,6 +201,7 @@ public class Sphere : MonoBehaviour {
 
         mesh.vertices  = vertices;
         mesh.normals   = normals;
+        mesh.uv        = uvs;
         mesh.triangles = triangles;
 
         GetComponent<MeshFilter>().sharedMesh = mesh;
--- first-simple-procedural-geometry/Assets/Square.cs
+++ first-simple-procedural-geometry/Assets/Square.cs
@@ -24,25 +24,35 @@ public class Square : MonoBehaviour {
         // };
 
         // Level-Of-Details (count of quads along one side)
-        float step     = (float)SIZE / LOD; // discrete quad's side size (optimization)
-        float halfSize = SIZE / 2f;         // hald of the SIZE (optimization)
-        int quadLOD    = (int)(LOD * LOD);  // LOD^2 (optimization)
+        float xyQuadSize = (float)SIZE / LOD; // discrete quad's side xy size (optimization)
+        float uvQuadSize = 1f / LOD;          // discrete quad's side uv size (optimization)
+        float halfSize   = SIZE / 2f;         // hald of the SIZE (optimization)
+        int quadLOD      = (int)(LOD * LOD);  // LOD^2 (optimization)
 
         Vector3[] vertices = new Vector3[4 * quadLOD]; // 4*LOD^2 vertices
         Vector3[] normals  = new Vector3[4 * quadLOD]; // 4*LOD^2 normals
+        Vector2[] uvs      = new Vector2[4 * quadLOD]; // UV coordinates for each vertex
         int[] triangles    = new int[2 * quadLOD * 3]; // 2*LOD^2 triangles (3 indexes each) each
 
         for (int i = 0; i < quadLOD; ++i) {
             int row    = i / (int)LOD;
             int column = i % (int)LOD;
-            float lux  = column * step - halfSize;
-            float rux  = (column + 1) * step - halfSize;
+            float lux  = column * xyQuadSize - halfSize;
+            float rux  = (column + 1) * xyQuadSize - halfSize;
             float ldx  = lux;
             float rdx  = rux;
-            float luy  = (row + 1) * step - halfSize;
+            float luy  = (row + 1) * xyQuadSize - halfSize;
             float ruy  = luy;
-            float ldy  = row * step - halfSize;
+            float ldy  = row * xyQuadSize - halfSize;
             float rdy  = ldy;
+            float luu  = column * uvQuadSize;
+            float ruu  = (column + 1) * uvQuadSize;
+            float ldu  = luu;
+            float rdu  = ruu;
+            float luv  = (row + 1) * uvQuadSize;
+            float ruv  = luv;
+            float ldv  = row * uvQuadSize;
+            float rdv  = ldv;
 
             vertices[i * 4]     = new Vector3(lux, luy, 0); // left-up
             vertices[i * 4 + 1] = new Vector3(rux, ruy, 0); // right-up
@@ -54,6 +64,11 @@ public class Square : MonoBehaviour {
             normals[i * 4 + 2] = Vector3.back; // left-down
             normals[i * 4 + 3] = Vector3.back; // right-down
 
+            uvs[i * 4]     = new Vector2(luu, luv); // left-up
+            uvs[i * 4 + 1] = new Vector2(ruu, ruv); // right-up
+            uvs[i * 4 + 2] = new Vector2(ldu, ldv); // left-down
+            uvs[i * 4 + 3] = new Vector2(rdu, rdv); // right-down
+
             triangles[i * 6]     = i * 4;     // left-up quad
             triangles[i * 6 + 1] = i * 4 + 1; // left-up quad
             triangles[i * 6 + 2] = i * 4 + 2; // left-up quad
@@ -64,6 +79,7 @@ public class Square : MonoBehaviour {
 
         mesh.vertices  = vertices;
         mesh.normals   = normals;
+        mesh.uv        = uvs;
         mesh.triangles = triangles;
 
         GetComponent<MeshFilter>().sharedMesh = mesh;
